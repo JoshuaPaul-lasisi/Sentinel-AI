1d272caf7b03e19e6bf80019c52aabc700e6a5eb
Tobi Joshua <jpaullasisi@gmail.com>
2025-11-02 00:42:26 +0100

    correcting api

--- api.py
+++ api.py
@@ -127,7 +127,3 @@ async def get_sample_transaction():
         "city": "Lagos",
         "txn_type": "transfer"
     }
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
